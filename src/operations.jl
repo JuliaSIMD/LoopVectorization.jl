09cdd9461783bb8024ea65b8afe27681d1c8082d
Chris Elrod <elrodc@gmail.com>
2020-02-12 00:20:47 -0500

    Add test for printing a store, and also to confirm presence of reduce_to_add instruction when reducing zeros.

--- src/operations.jl
+++ src/operations.jl
@@ -35,11 +35,11 @@ Base.convert(::Type{ArrayReference}, ref::ArrayReferenceMeta) = ref.ref
 Base.:(==)(x::ArrayReference, y::ArrayReference) = isequal(x, y)
 Base.:(==)(x::ArrayReferenceMeta, y::ArrayReferenceMeta) = isequal(x.ref, y.ref) && x.ptr === y.ptr
 
-
-Base.:(==)(x::ArrayReference, y::ArrayReferenceMeta) = x == y.ref
-Base.:(==)(x::ArrayReferenceMeta, y::ArrayReference) = x.ref == y
-Base.:(==)(x::ArrayReference, y) = false
-Base.:(==)(x::ArrayReferenceMeta, y) = false
+# Errors preferable than silently working?
+# Base.:(==)(x::ArrayReference, y::ArrayReferenceMeta) = x == y.ref
+# Base.:(==)(x::ArrayReferenceMeta, y::ArrayReference) = x.ref == y
+# Base.:(==)(x::ArrayReference, y) = false
+# Base.:(==)(x::ArrayReferenceMeta, y) = false
 
 @enum OperationType begin
     constant
--- test/printmethods.jl
+++ test/printmethods.jl
@@ -10,4 +10,20 @@
     @test occursin("Operation[", s)
     @test occursin("s = 0", s)
     @test occursin("s = LoopVectorization.vfmadd", s)
+
+    AmulBq = :(for m ∈ 1:M, n ∈ 1:N
+                   C[m,n] = zero(eltype(B))
+                   for k ∈ 1:K
+                       C[m,n] += A[m,k] * B[k,n]
+                   end
+               end)
+    lsAmulB = LoopVectorization.LoopSet(AmulBq);
+    println(io, LoopVectorization.operations(lsAmulB))
+    s = String(take!(io))
+    @test occursin("Operation[", s)
+    @test occursin("C[m, n] = ", s)
+    @test occursin(" = A[m, k]", s)
+    @test occursin(" = B[k, n]", s)
+    @test occursin(" = LoopVectorization.vfmadd", s)
+    @test occursin(" = LoopVectorization.reduce_to_add", s)
 end
