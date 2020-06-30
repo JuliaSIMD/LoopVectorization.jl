696a0d78d7fce6f9fa58a4eb75ab83ea2a97a684
Chris Elrod <elrodc@gmail.com>
2020-06-29 20:25:48 -0400

    Add src/getconstindexes.jl

---
+++ src/getconstindexes.jl
@@ -0,0 +1,9 @@
+@inline second(x) = @inbounds getindex(x, 2)
+@inline third(x) = @inbounds getindex(x, 3)
+@inline fourth(x) = @inbounds getindex(x, 4)
+@inline fifth(x) = @inbounds getindex(x, 5)
+@inline sixth(x) = @inbounds getindex(x, 6)
+@inline seventh(x) = @inbounds getindex(x, 7)
+@inline eighth(x) = @inbounds getindex(x, 8)
+@inline ninth(x) = @inbounds getindex(x, 9)
+
