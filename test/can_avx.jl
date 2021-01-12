c204d59c9e630d05df22d77d45424e68525a24ac
Chris Elrod <elrodc@gmail.com>
2021-01-12 07:54:17 -0500

    Make sure relu, sigmoid_fast, and tanh_fast `can_Avx`

--- test/can_avx.jl
+++ test/can_avx.jl
@@ -11,7 +11,11 @@
     @test LoopVectorization.ArrayInterface.can_avx(Base.FastMath.add_fast)
     @test LoopVectorization.ArrayInterface.can_avx(/)
     @test LoopVectorization.ArrayInterface.can_avx(sqrt)
+    @test LoopVectorization.ArrayInterface.can_avx(tanh_fast)
+    @test LoopVectorization.ArrayInterface.can_avx(sigmoid_fast)
+    @test LoopVectorization.ArrayInterface.can_avx(VectorizationBase.relu)
     @test !LoopVectorization.ArrayInterface.can_avx(clenshaw)
+    @test !LoopVectorization.ArrayInterface.can_avx(println)
 
 end
 
