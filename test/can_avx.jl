40c425af88b5e739817b744e4f4b167b408f8eed
MilesCranmer <miles.cranmer@gmail.com>
2022-09-18 14:38:44 -0400

    Remove `@turbo safe=true` tests from `can_avx.jl`

--- test/can_avx.jl
+++ test/can_avx.jl
@@ -3,10 +3,8 @@
 
 @testset "can_avx" begin
 
-  import SpecialFunctions
-
   good_operators = [log, log1p, exp, +, -, Base.FastMath.add_fast, /, sqrt, tanh_fast, sigmoid_fast, LoopVectorization.relu]
-  bad_operators = [clenshaw, println, SpecialFunctions.gamma]
+  bad_operators = [clenshaw, println]
 
   for op in good_operators
     @test LoopVectorization.can_avx(op)
@@ -14,20 +12,5 @@
   for op in bad_operators
     @test !LoopVectorization.can_avx(op)
   end
-
-
-  # Test safe @turbo
-  x = Float32.(1:0.1:10)
-  y = similar(x)
-  truth = similar(x)
-
-  @turbo safe=true for i in indices(x)
-      y[i] = SpecialFunctions.gamma(x[i])
-  end
-  for i in indices(x)
-      truth[i] = SpecialFunctions.gamma(x[i])
-  end
-
-  @test y ≈ truth
 
 end
