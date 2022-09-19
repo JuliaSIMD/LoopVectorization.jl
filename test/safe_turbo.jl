9568ba9c3d1e11ac6123916a59eb83e7fc0de4d7
MilesCranmer <miles.cranmer@gmail.com>
2022-09-19 13:25:00 -0400

    Ensure that function names in safe test are unique

--- test/safe_turbo.jl
+++ test/safe_turbo.jl
@@ -1,36 +1,41 @@
+_f1(a) = SpecialFunctions.gamma(a)
+_f2(a) = exp(a)
+_f3(a, b) = a + SpecialFunctions.gamma(b)
+_f4(a, b) = a + exp(b)
+_f5(a, b) = a + SpecialFunctions.gamma(b)
+_f6(a, b) = a + SpecialFunctions.gamma(b)
+
 @testset "Safe @turbo" begin
 
+  using LoopVectorization
+  using Test
   import SpecialFunctions
 
   # All methods, both `can_avx` and `can_turbo`, should recognize that
   # `gamma` is not AVX-able
-  f1(x) = SpecialFunctions.gamma(x)
 
   @test !LoopVectorization.ArrayInterface.can_avx(SpecialFunctions.gamma)
   @test !LoopVectorization.can_turbo(SpecialFunctions.gamma, Val(1))
-  @test !LoopVectorization.can_turbo(f1, Val(1))
+  @test !LoopVectorization.can_turbo(_f1, Val(1))
 
   # `can_avx` is not able to detect that a function `f` which is just
   # `gamma` can be AVX'd, but `can_turbo` can:
-  f2(x) = exp(x)
 
   @test LoopVectorization.ArrayInterface.can_avx(exp)
-  @test !LoopVectorization.ArrayInterface.can_avx(f2)
+  @test !LoopVectorization.ArrayInterface.can_avx(_f2)
   @test LoopVectorization.can_turbo(exp, Val(1))
-  @test LoopVectorization.can_turbo(f2, Val(1))
+  @test LoopVectorization.can_turbo(_f2, Val(1))
 
   # Next, we test with multiple arguments:
-  g1(x, y) = x + SpecialFunctions.gamma(y)
-  @test !LoopVectorization.can_turbo(g1, Val(2))
-  g2(x, y) = x + exp(y)
-  @test LoopVectorization.can_turbo(g2, Val(2))
+  @test !LoopVectorization.can_turbo(_f3, Val(2))
+  @test LoopVectorization.can_turbo(_f4, Val(2))
 
   x = Float32.(1.05:0.1:10)
   y = Float32.(0.55:0.1:10.5)
   z = similar(x)
   truth = similar(x)
 
-  @turbo safe=true for i in indices(x)
+  LoopVectorization.@turbo safe=true for i in indices(x)
       z[i] = SpecialFunctions.gamma(x[i])
   end
   for i in indices(x)
@@ -38,14 +43,13 @@
   end
   @test z ≈ truth
   
-  f3(x, y) = x + SpecialFunctions.gamma(y)
-  @turbo safe=true for i in indices(x)
-      z[i] = f3(x[i], y[i])
+  LoopVectorization.@turbo safe=true for i in indices(x)
+      z[i] = _f5(x[i], y[i])
   end
-  f4(x, y) = x + SpecialFunctions.gamma(y)
   for i in indices(x)
-      truth[i] = f4(x[i], y[i])
+      truth[i] = _f6(x[i], y[i])
   end
   @test z ≈ truth
 
 end
+
