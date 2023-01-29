2b50253f8040740b8e4e1369de97422e8b446afc
Chris Elrod <elrodc@gmail.com>
2023-01-29 13:01:43 -0500

    Fix zygote tests

--- ext/ForwardDiffExt.jl
+++ ext/ForwardDiffExt.jl
@@ -1,7 +1,24 @@
 module ForwardDiffExt
 import ForwardDiff, ChainRulesCore
 using SIMDDualNumbers, LoopVectorization
-using LoopVectorization: AbstractSIMD, AbstractStridedPointer, relu
+using LoopVectorization:
+  AbstractSIMD,
+  AbstractStridedPointer,
+  relu,
+  vmap,
+  VectorizationBase,
+  vmapt,
+  vmapnt,
+  vmapntt,
+  MM,
+  StaticInt,
+  vadd_nw,
+  vsub_nsw,
+  vload,
+  mask,
+  vfnmadd_fast,
+  mul_fast
+using VectorizationBase: zero_offsets
 
 @generated function init_dual(v::Tuple{Vararg{AbstractSIMD,A}}) where {A}
   res = Expr(:tuple)
@@ -76,32 +93,33 @@ function ∂vmap_singlethread!(
   args::Vararg{DenseArray{<:Base.HWReal},A}
 ) where {F,T<:Base.HWReal,A}
   N = length(y)
-  ptry = VectorizationBase.zero_offsets(stridedpointer(y))
-  ptrargs = VectorizationBase.zero_offsets.(stridedpointer.(args))
-  ptr∂y = VectorizationBase.zero_offsets.(stridedpointer.(∂y))
-
+  ptry = zero_offsets(stridedpointer(y))
+  ptrargs = map(zero_offsets, map(stridedpointer, args))
+  ptr∂y = map(zero_offsets, map(stridedpointer, ∂y))
   i = 0
   V = VectorizationBase.pick_vector_width(T)
   W = Int(V)
-  st = VectorizationBase.static_sizeof(T)
-  zero_index = MM{W}(StaticInt(0), st)
   while i < vsub_nsw(N, ((W << 2) - 1))
     index = VectorizationBase.Unroll{1,W,4,1,W,zero(UInt)}((i,))
-    v = f(init_dual(vload.(ptrargs, index))...)
+    v = f(init_dual(map(Base.Fix2(vload, index), ptrargs))...)
     dual_store!(ptr∂y, ptry, v, index)
     i = vadd_nw(i, 4W)
   end
   while i < vsub_nsw(N, (W - 1))
-    vᵣ = f(init_dual(vload.(ptrargs, ((MM{W}(i),),)))...)
+    loader = Base.Fix2(vload, (MM{W}(i),))
+    vᵣ = f(init_dual(map(loader, ptrargs))...)
     dual_store!(ptr∂y, ptry, vᵣ, (MM{W}(i),))
     i = vadd_nw(i, W)
   end
   if i < N
     m = mask(T, N & (W - 1))
+    mloader = let i = i, m = m
+      p -> vload(p, (MM{W}(i),), m)
+    end
     dual_store!(
       ptr∂y,
       ptry,
-      f(init_dual(vload.(ptrargs, ((MM{W}(i),),), m))...),
+      f(init_dual(map(mloader, ptrargs))...),
       (MM{W}(i),),
       m
     )
@@ -147,7 +165,7 @@ for f in (:vmapt, :vmapnt, :vmapntt)
     f::F,
     args::Vararg{Any,K}
   ) where {F,K}
-    ChainRulesCore.rrule(typeof(vmap), f, args...)
+    ChainRulesCore.rrule(typeof($vmap), f, args...)
   end
 end
 end
