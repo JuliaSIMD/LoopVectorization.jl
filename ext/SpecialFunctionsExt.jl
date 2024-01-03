a4a160f376d53fe4c44e16eafbf838bb243e659f
Chris Elrod <elrodc@gmail.com>
2024-01-03 12:44:26 -0500

    Deprecate LV for Julia >= 1.11-DEV (#519)
    * Deprecate LV for Julia >= 1.11-DEV
    
    * fix for tests to be able to run
    
    * README comment and bump version.
    
    * esc

--- ext/SpecialFunctionsExt.jl
+++ ext/SpecialFunctionsExt.jl
@@ -1,6 +1,8 @@
 module SpecialFunctionsExt
+if VERSION < v"1.11-DEV"
 using SpecialFunctions
 using LoopVectorization: VectorizationBase
 using LoopVectorization: AbstractSIMD
 @inline SpecialFunctions.erf(x::AbstractSIMD) = VectorizationBase.verf(float(x))
+end
 end
