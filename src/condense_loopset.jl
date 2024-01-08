5cf8ddb56695babd8de05a5d626e4e25c9adbfa5
Tim Besard <tim.besard@gmail.com>
2024-01-08 20:29:46 +0100

    Remove broken register passing optimization.

--- src/condense_loopset.jl
+++ src/condense_loopset.jl
@@ -4,71 +4,6 @@
 Base.:|(u::Unsigned, it::IndexType) = u | UInt8(it)
 Base.:(==)(u::Unsigned, it::IndexType) = (u % UInt8) == UInt8(it)
 
-function _append_fields!(t::Expr, body::Expr, sym::Symbol, ::Type{T}) where {T}
-  for f ∈ 1:fieldcount(T)
-    TF = fieldtype(T, f)
-    Base.issingletontype(TF) && continue
-    gfcall = Expr(:call, getfield, sym, f)
-    if fieldcount(TF) ≡ 0
-      push!(t.args, gfcall)
-    elseif TF <: DataType
-      push!(t.args, Expr(:call, Expr(:curly, lv(:StaticType), gfcall)))
-    else
-      newsym = gensym(sym)
-      push!(body.args, Expr(:(=), newsym, gfcall))
-      _append_fields!(t, body, newsym, TF)
-    end
-  end
-  return nothing
-end
-@generated function flatten_to_tuple(r::T) where {T}
-  body = Expr(:block, Expr(:meta, :inline))
-  t = Expr(:tuple)
-  if Base.issingletontype(T)
-    nothing
-  elseif fieldcount(T) ≡ 0
-    push!(t.args, :r)
-  elseif T <: DataType
-    push!(t.args, Expr(:call, Expr(:curly, lv(:StaticType), :r)))
-  else
-    _append_fields!(t, body, :r, T)
-  end
-  push!(body.args, t)
-  body
-end
-function rebuild_fields(offset::Int, ::Type{T}) where {T}
-  call = (T <: Tuple) ? Expr(:tuple) : Expr(:new, T)
-  for f ∈ 1:fieldcount(T)
-    TF = fieldtype(T, f)
-    if Base.issingletontype(TF)
-      push!(call.args, TF.instance)
-    elseif fieldcount(TF) ≡ 0
-      push!(call.args, Expr(:call, getfield, :t, (offset += 1)))
-    elseif TF <: DataType
-      push!(
-        call.args,
-        Expr(:call, lv(:gettype), Expr(:call, getfield, :t, (offset += 1)))
-      )
-    else
-      arg, offset = rebuild_fields(offset, TF)
-      push!(call.args, arg)
-    end
-  end
-  return call, offset
-end
-@generated function reassemble_tuple(::Type{T}, t::Tuple) where {T}
-  if Base.issingletontype(T)
-    return T.instance
-  elseif fieldcount(T) ≡ 0
-    call = Expr(:call, getfield, :t, 1)
-  elseif T <: DataType
-    call = Expr(:call, lv(:gettype), Expr(:call, getfield, :t, 1))
-  else
-    call, _ = rebuild_fields(0, T)
-  end
-  Expr(:block, Expr(:meta, :inline), call)
-end
-
 """
     ArrayRefStruct
 
@@ -893,9 +828,9 @@ function generate_call_types(
       )
     )
     if manyarg
-      push!(q.args, Expr(:call, lv(:flatten_to_tuple), vargsym))
+      push!(q.args, vargsym)
     else
-      push!(q.args, Expr(:(...), Expr(:call, lv(:flatten_to_tuple), vargsym)))
+      push!(q.args, Expr(:(...), vargsym))
     end
     Expr(:block, Expr(:(=), vargsym, Expr(:tuple, lbarg, extra_args)))
   end
