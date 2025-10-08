d6e39b13e3ffcc433927b20af3599c601f67a489
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-10-08 01:37:53 -0400

    Create ForwardDiffNNlibExt.jl

---
+++ ext/ForwardDiffNNlibExt.jl
@@ -0,0 +1,42 @@
+module ForwardDiffNNlibExt
+import ForwardDiff
+using LoopVectorization, VectorizationBase, SLEEFPirates, ForwardDiff, NNlib
+
+@generated function NNlib.relu(
+  x::ForwardDiff.Dual{T,<:LoopVectorization.AbstractSIMD,N}
+) where {T,S,N}
+  quote
+    $(Expr(:meta, :inline))
+    v = x.value
+    z = zero(v)
+    cmp = v < z
+    r = ifelse(cmp, z, v)
+    p = x.partials
+    ForwardDiff.Dual{T}(
+      r,
+      ForwardDiff.Partials(Base.Cartesian.@ntuple $N n -> ifelse(cmp, z, p[n]))
+    )
+  end
+end
+
+@generated function NNlib.leakyrelu(
+  x::ForwardDiff.Dual{T,<:LoopVectorization.AbstractSIMD,N},
+  a = 0.01
+) where {T,S,N}
+  quote
+    $(Expr(:meta, :inline))
+    v = x.value
+    z = zero(v)
+
+    α = convert(typeof(v), a)
+    cmp = v < z
+    r = ifelse(cmp, α * v, v)
+    p = x.partials
+    ForwardDiff.Dual{T}(
+      r,
+      ForwardDiff.Partials(Base.Cartesian.@ntuple $N n -> ifelse(cmp, α * p[n], p[n]))
+    )
+  end
+end
+
+end
